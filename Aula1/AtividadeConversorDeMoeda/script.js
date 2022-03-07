5b0d18a58ec54d00a9f41e695c79ff941bbf3448
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-03-07 18:46:50 -0300

    BaseDaAula1

---
+++ Aula1/AtividadeConversorDeMoeda/script.js
@@ -0,0 +1,23 @@
+function conversor(){
+
+    const valorEmReais = input(parseInt("Quantos reais?"));
+    let convert = (valorEmReais/5.11);
+    return console.log("R$ " + valorEmReais+ " = $"+convert )
+
+    }
+    
+    console.log (conversor ());
+    
+    
+    // isto é um comentario. 
+    // usei fuction para fazer a media.
+    
+    // revisão:
+    // variaveis, strings, console.log, toFixed, operações matematias, concatenação, console.log, let, function.
+    
+    //desafio:
+    // verficar se o aluno passou ou não de ano.
+    //Estilizar o fundo da pagina;
+    // escrever na pagina o que estamos escrevendo no console.
+    // conversor de temperatura celsius a fareraid.
+    //nota fixada com toda conta, divisão de media + tofixed
