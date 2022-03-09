ce25051597c08de24efc79dfcaab69d509bc7fe0
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-03-09 18:03:15 -0300

    Reajuste: Atividade ConversorMoeda
    atividade feita totalmente por mim finalmente ajeitada

--- Aula1/AtividadeConversorDeMoeda/script.js
+++ Aula1/AtividadeConversorDeMoeda/script.js
@@ -1,8 +1,8 @@
-document.getElementById("button-convert").onclick = function(){
+function conversor(){
 
     let valor = document.getElementById("barrinha-valor-real").value; 
     let convert = (parseFloat(valor/5.11)).toFixed(2);
-    let resultado = document.querySelector('span');
+    let resultado = document.getElementById('resultadoConversão');
     resultado.innerHTML= ( "R$"+valor+ " = U$"+convert );
     }
     
